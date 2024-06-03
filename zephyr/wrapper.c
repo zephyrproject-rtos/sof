a77215d6ccce2464ea63b1740318aeebad09d310
Yong Cong Sin <yongcong.sin@gmail.com>
2024-06-04 01:05:52 +0800

    zephyr: change `z_arch_esf_t` to `struct arch_esf`
    `z_arch_esf_t` has been deprecated, use `struct arch_esf`
    instead.
    
    Signed-off-by: Yong Cong Sin <ycsin@meta.com>

--- zephyr/wrapper.c
+++ zephyr/wrapper.c
@@ -328,7 +328,7 @@ int poll_for_register_delay(uint32_t reg, uint32_t mask,
 }
 
 void k_sys_fatal_error_handler(unsigned int reason,
-			       const z_arch_esf_t *esf)
+			       const struct arch_esf *esf)
 {
 	ARG_UNUSED(esf);
 	LOG_PANIC();
